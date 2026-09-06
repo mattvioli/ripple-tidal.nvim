5b8d72c413a695f6eb40ed26c15f862f226cd2a6
mattvioli <mattvioli@gmail.com>
2026-09-06 17:38:47 +1000

    fix: warn when sending to a REPL that isn't running

--- lua/tidal/core/message.lua
+++ lua/tidal/core/message.lua
@@ -1,25 +1,50 @@
 local state = require("tidal.core.state")
+local notify = require("tidal.util.notify")
 
 local M = {}
+
+local warned = { tidal = false, sclang = false }
+
+local function repl(name)
+  if name == "sclang" then
+    return state.sclang
+  end
+  return state.ghci
+end
+
+local function check(name)
+  if repl(name) then
+    warned[name] = false
+    return true
+  end
+  if state.launching then
+    return false
+  end
+  if not warned[name] then
+    warned[name] = true
+    notify.warn(string.format("%s REPL is not running; send dropped. Launch with ':TidalLaunch'", name))
+  end
+  return false
+end
 
 M.tidal = {}
 
 function M.tidal.send(text)
-  if not state.ghci then
+  if not check("tidal") then
     return
   end
   state.ghci:send(text)
 end
 
 function M.tidal.send_line(text)
-  if not state.ghci then
+  if not check("tidal") then
     return
   end
   state.ghci:send_line(text)
 end
 
 function M.tidal.send_multiline(lines)
-  if not state.ghci then
+  if not check("tidal") then
     return
   end
   state.ghci:send_multiline(lines)
@@ -28,21 +53,21 @@ end
 M.sclang = {}
 
 function M.sclang.send(text)
-  if not state.sclang then
+  if not check("sclang") then
     return
   end
   state.sclang:send(text)
 end
 
 function M.sclang.send_line(text)
-  if not state.sclang then
+  if not check("sclang") then
     return
   end
   state.sclang:send_line(text)
 end
 
 function M.sclang.send_multiline(lines)
-  if not state.sclang then
+  if not check("sclang") then
     return
   end
   state.sclang:send_multiline(lines)
